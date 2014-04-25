661e129bd58b339d97e526151db331dc3f049ac6
martino <manda.mgf@gmail.com>
2014-04-25 17:04:59 +0200

    Added Procedural Node

---
+++ OpenView/src/gui/components/ovprocedural/OVProceduralNode.java
@@ -0,0 +1,18 @@
+package gui.components.ovprocedural;
+
+import gui.components.ovnode.OVNodeComponent;
+import gui.interfaces.OVContainer;
+
+public class OVProceduralNode extends OVNodeComponent {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2159211622124885366L;
+
+	public OVProceduralNode(OVContainer father) {
+		super(father);
+		// TODO Auto-generated constructor stub
+	}
+
+}
