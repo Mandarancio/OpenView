17b764a456a484206f4629e1fbe581a75826b770
Mandarancio <manda.mgf@gmail.com>
2014-04-29 21:19:11 +0200

    Finnaly recursive function added!

--- OpenView/src/ovscript/IFBlock.java
+++ OpenView/src/ovscript/IFBlock.java
@@ -8,188 +8,210 @@ package ovscript;
 import java.util.HashMap;
 
 import core.Value;
+import java.util.ArrayList;
 
 /**
- * 
+ *
  * @author martino
  */
 public class IFBlock extends AbstractBlock implements CodeBlock {
-
-	private Block condition_;
-	private Block else_;
-	private Block body_;
-	private CodeBlock parent_;
-	private HashMap<String, Var> variables_ = new HashMap<>();
-
-	public IFBlock(CodeBlock parent) {
-		super("IF");
-		parent_ = parent;
-	}
-
-	@Override
-	public Value run(CodeBlock i) {
-		return new Value(Void.TYPE);
-	}
-
-	@Override
-	public Block next() {
-		Value v = condition_.run(this);
-		boolean b = false;
-		try {
-			b = v.getBoolean();
-		} catch (Exception e) {
-		}
-		if (b) {
-			return body_;
-		} else {
-
-			if (else_ != null) {
-
-				return else_;
-			} else
-				return super.next();
-		}
-	}
-
-	public void setCondition(Block b) {
-		condition_ = b;
-	}
-
-	public void setElse(Block b) {
-		else_ = b;
-	}
-
-	void setBody(Block first) {
-		body_ = first;
-	}
-
-	@Override
-	public CodeBlock parent() {
-		return parent_;
-	}
-
-	@Override
-	public ReturnStruct parse(String[] lines) {
-		Block first = null;
-		Block last = null;
-		int i = 0;
-		while (i < lines.length) {
-			String copy[] = new String[lines.length - i];
-			System.arraycopy(lines, i, copy, 0, copy.length);
-
-			ReturnStruct rs = Parser.parseLine(this, lines[i], copy);
-			Block b = rs.block;
-			i += rs.lines;
-
-			if (b instanceof ELSEBlock) {
-				this.setElse(b);
-
-				break;
-			} else if (b instanceof ENDBlock) {
-				this.setNext(b);
-				break;
-			} else {
-				if (b != null) {
-					if (first == null) {
-						first = b;
-						last = b;
-						this.setBody(first);
-					} else {
-						last.setNext(b);
-						last = b;
-					}
-				}
-			}
-
-		}
-		return new ReturnStruct(this, i + 1);
-	}
-
-	@Override
-	public void setNext(Block b) {
-		super.setNext(b);
-		getLast(body_).setNext(b);
-		if (else_ != null) {
-			else_.setNext(b);
-		}
-	}
-
-	private Block getLast(Block b) {
-		Block bb = b;
-		while (bb != null) {
-			if (bb instanceof IFBlock) {
-				Block n=((IFBlock) bb).superNext();
-				if (n!=null)
-					bb = n;
-				else 
-					return bb;
-			} else if (bb.next() != null){
-				bb = bb.next();
-			} else
-				return bb;
-		}
-		return b;
-	}
-
-	public Block superNext() {
-		return super.next();
-	}
-
-	@Override
-	public Value runBlock(Block b) {
-		return run(this);
-	}
-
-	@Override
-	public HashMap<String, Var> variableStack() {
-		HashMap<String, Var> vars = new HashMap<>(variables_);
-		vars.putAll(parent_.variableStack());
-		return vars;
-	}
-
-	@Override
-	public void debug(String code) {
-		DebugManager.debug(code, this);
-	}
-
-	@Override
-	public boolean isDebug() {
-		return parent_.isDebug();
-	}
-
-	@Override
-	public void putVar(String name, Var v) {
-		variables_.put(name, v);
-	}
-
-	@Override
-	public Var getVar(String name) {
-		Var v = variables_.get(name);
-		if (v == null)
-			v = parent_.getVar(name);
-		return v;
-	}
-
-	@Override
-	public void endRun() {
-		__end = true;
-	}
-
-	@Override
-	public HashMap<String, Var> localVariableStack() {
-		return variables_;
-	}
-
-	@Override
-	public void addFunctionDefinition(FunctionBlock f) {
-		System.err
-				.println("something wrong! you can not define a function in a "
-						+ getClass().getSimpleName());
-	}
-
-	@Override
-	public FunctionBlock getFunctionDefinition(String past, int nargs) {
-		return parent_.getFunctionDefinition(past, nargs);
-	}
-
+    
+    private Block condition_;
+    private Block else_;
+    private Block body_;
+    private CodeBlock parent_;
+    private HashMap<String, Var> variables_ = new HashMap<>();
+    private String[] code_;
+    
+    public IFBlock(CodeBlock parent) {
+        super("IF");
+        parent_ = parent;
+    }
+    
+    @Override
+    public Value run(CodeBlock i) {
+        return new Value(Void.TYPE);
+    }
+    
+    @Override
+    public Block next() {
+        Value v = condition_.run(this);
+        boolean b = false;
+        try {
+            b = v.getBoolean();
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if (b) {
+            return parseForRun();
+        } else {
+            
+            if (else_ != null) {
+                
+                return else_;
+            } else {
+                return super.next();
+            }
+        }
+    }
+    
+    public void setCondition(Block b) {
+        condition_ = b;
+    }
+    
+    public void setElse(Block b) {
+        else_ = b;
+    }
+    
+    void setBody(Block first) {
+        body_ = first;
+    }
+    
+    @Override
+    public CodeBlock parent() {
+        return parent_;
+    }
+    
+    private Block parseForRun() {
+        int i = 0;
+        Block first = null;
+        Block last = null;
+        while (i < code_.length) {
+            String copy[] = new String[code_.length - i];
+            System.arraycopy(code_, i, copy, 0, copy.length);
+            
+            ReturnStruct rs = Parser.parseLine(this, code_[i], copy);
+            Block b = rs.block;
+            i += rs.lines;
+            
+            if (b instanceof ELSEBlock) {
+                this.setElse(b);
+                
+                break;
+            } else if (b instanceof ENDBlock) {
+                this.setNext(b);
+                break;
+            } else {
+                if (b != null) {
+                    if (first == null) {
+                        first = b;
+                        last = b;
+                        this.setBody(first);
+                    } else {
+                        last.setNext(b);
+                        last = b;
+                    }
+                }
+            }
+            
+        }
+        last.setNext(super.next());
+        return first;
+    }
+    
+    @Override
+    public ReturnStruct parse(String[] lines) {
+        int i = 0;
+        int c = 1;
+        ArrayList<String> code = new ArrayList<String>();
+        while (i < lines.length) {
+            String line = Parser.clean(lines[i]);
+            
+            if (line.startsWith("if ") || line.startsWith("for ") || line.startsWith("while ") || line.startsWith("function ")) {
+                c++;
+            } else if (line.equals("end")) {
+                c--;
+            }
+            
+            
+            if (c == 1 && line.startsWith("elif ") || line.startsWith("else")) {
+                String copy[] = new String[lines.length - i];
+                System.arraycopy(lines, i, copy, 0, copy.length);
+                ReturnStruct rs = Parser.parseLine(this, line, copy);
+                i += rs.lines;
+                this.setElse(rs.block);
+                this.code_ = code.toArray(new String[code.size()]);
+                return new ReturnStruct(this, i + 1);
+            } else if (c == 0) {
+                this.code_ = code.toArray(new String[code.size()]);
+                return new ReturnStruct(this, i + 1);
+            } else i++;
+            code.add(line);
+            
+        }
+        System.err.println("SOMETHING VERY WRONG!");
+        return new ReturnStruct(this, i + 1);
+    }
+    
+    @Override
+    public void setNext(Block b) {
+        super.setNext(b);
+        if (else_ != null) {
+            else_.setNext(b);
+        }
+    }
+    
+    public Block superNext() {
+        return super.next();
+    }
+    
+    @Override
+    public Value runBlock(Block b) {
+        return run(this);
+    }
+    
+    @Override
+    public HashMap<String, Var> variableStack() {
+        HashMap<String, Var> vars = new HashMap<>(variables_);
+        vars.putAll(parent_.variableStack());
+        return vars;
+    }
+    
+    @Override
+    public void debug(String code) {
+        DebugManager.debug(code, this);
+    }
+    
+    @Override
+    public boolean isDebug() {
+        return parent_.isDebug();
+    }
+    
+    @Override
+    public void putVar(String name, Var v) {
+        variables_.put(name, v);
+    }
+    
+    @Override
+    public Var getVar(String name) {
+        Var v = variables_.get(name);
+        if (v == null) {
+            v = parent_.getVar(name);
+        }
+        return v;
+    }
+    
+    @Override
+    public void endRun() {
+        __end = true;
+    }
+    
+    @Override
+    public HashMap<String, Var> localVariableStack() {
+        return variables_;
+    }
+    
+    @Override
+    public void addFunctionDefinition(FunctionBlock f) {
+        System.err
+                .println("something wrong! you can not define a function in a "
+                        + getClass().getSimpleName());
+    }
+    
+    @Override
+    public FunctionBlock getFunctionDefinition(String past, int nargs) {
+        return parent_.getFunctionDefinition(past, nargs);
+    }
+    
 }
--- OpenView/src/ovscript/WHILEBlock.java
+++ OpenView/src/ovscript/WHILEBlock.java
@@ -3,127 +3,149 @@ package ovscript;
 import java.util.HashMap;
 
 import core.Value;
+import java.util.ArrayList;
 
 public class WHILEBlock extends AbstractBlock implements CodeBlock {
 
-	private Block body_;
-	private Block condition_;
+    private Block body_;
+    private Block condition_;
 
-	private CodeBlock parent_;
-	private HashMap<String, Var> variables_ = new HashMap<>();
+    private CodeBlock parent_;
+    private HashMap<String, Var> variables_ = new HashMap<>();
+    private String[] code_;
 
-	public WHILEBlock(CodeBlock parent, Block c) {
-		super("while");
-		condition_ = c;
-		parent_ = parent;
-	}
+    public WHILEBlock(CodeBlock parent, Block c) {
+        super("while");
+        condition_ = c;
+        parent_ = parent;
+    }
 
-	@Override
-	public Value run(CodeBlock i) {
-		try {
-			while (!__end && condition_.run(i).getBoolean()) {
-				runBlock(body_, this);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return new Value();
-	}
+    @Override
+    public Value run(CodeBlock i) {
+        try {
+            if (body_ == null) {
+                body_ = parseForRun();
+            }
+            while (!__end && condition_.run(i).getBoolean()) {
+                runBlock(body_, this);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new Value();
+    }
 
-	public Block getBody() {
-		return body_;
-	}
+    @Override
+    public CodeBlock parent() {
+        return parent_;
+    }
 
-	public void setBody(Block body_) {
-		this.body_ = body_;
-	}
+    private Block parseForRun() {
+        int i = 0;
+        Block b = null;
+        String lines[] = code_;
+        
+        Block first=null;
+        while (i < lines.length) {
+            String copy[] = new String[lines.length - i];
+            System.arraycopy(lines, i, copy, 0, copy.length);
+            ReturnStruct rs = Parser.parseLine(this, lines[i], copy);
+            if (rs.block != null) {
+                if (b == null) {
+                    b = rs.block;
+                    first=rs.block;
+                } else {
+                    b.setNext(rs.block);
+                    b = rs.block;
+                }
+            }
+            i += rs.lines;
+        }
+        return first;
+    }
 
-	@Override
-	public CodeBlock parent() {
-		return parent_;
-	}
+    @Override
+    public ReturnStruct parse(String[] lines) {
+        int i = 0;
+        int c = 1;
+        ArrayList<String> code = new ArrayList<String>();
+        while (i < lines.length) {
+            String line = Parser.clean(lines[i]);
+            
+            if (line.startsWith("if ") || line.startsWith("for ") || line.startsWith("while ") || line.startsWith("function ")) {
+                c++;
+            } else if (line.equals("end")) {
+                c--;
+            }
+            
+            if (c == 0) {
+                code_ = code.toArray(new String[code.size()]);
+                return new ReturnStruct(this, i + 1);
+            }
+            
+            code.add(line);
+            i++;
+        }
 
-	@Override
-	public ReturnStruct parse(String[] lines) {
-		int i = 0;
-		Block b = null;
-		while (i < lines.length) {
-			String copy[] = new String[lines.length - i];
-			System.arraycopy(lines, i, copy, 0, copy.length);
-			ReturnStruct rs = Parser.parseLine(this, lines[i], copy);
-			if (rs.block != null) {
-				if (b == null) {
-					b = rs.block;
-					this.setBody(b);
-				} else {
-					b.setNext(rs.block);
-					b = rs.block;
-				}
-			}
-			i += rs.lines;
-			if (b instanceof ENDBlock) {
-				this.setNext(b);
-				break;
-			}
-		}
-		return new ReturnStruct(this, i + 1);
-	}
+        return new ReturnStruct(this, i + 1);
+    }
 
-	@Override
-	public Value runBlock(Block b) {
-		return run(this);
-	}
+    @Override
+    public Value runBlock(Block b) {
+        return run(this);
+    }
 
-	@Override
-	public HashMap<String, Var> variableStack() {
-		HashMap<String, Var> vars = new HashMap<>(variables_);
-		vars.putAll(parent_.variableStack());
-		return vars;
-	}
+    @Override
+    public HashMap<String, Var> variableStack() {
+        HashMap<String, Var> vars = new HashMap<>(variables_);
+        vars.putAll(parent_.variableStack());
+        return vars;
+    }
 
-	@Override
-	public void debug(String code) {
-		DebugManager.debug(code, this);
-	}
+    @Override
+    public void debug(String code) {
+        DebugManager.debug(code, this);
+    }
 
-	@Override
-	public boolean isDebug() {
-		return parent_.isDebug();
-	}
+    @Override
+    public boolean isDebug() {
+        return parent_.isDebug();
+    }
 
-	@Override
-	public void putVar(String name, Var v) {
-		variables_.put(name, v);
-	}
+    @Override
+    public void putVar(String name, Var v) {
+        variables_.put(name, v);
+    }
 
-	@Override
-	public Var getVar(String name) {
-		Var v = variables_.get(name);
-		if (v == null)
-			v = parent_.getVar(name);
-		return v;
-	}
+    @Override
+    public Var getVar(String name) {
+        Var v = variables_.get(name);
+        if (v == null) {
+            v = parent_.getVar(name);
+        }
+        return v;
+    }
 
-	@Override
-	public void endRun() {
-		__end = true;
-	}
+    @Override
+    public void endRun() {
+        __end = true;
+    }
 
-	@Override
-	public HashMap<String, Var> localVariableStack() {
-		return variables_;
-	}
+    @Override
+    public HashMap<String, Var> localVariableStack() {
+        return variables_;
+    }
 
-	@Override
-	public void addFunctionDefinition(FunctionBlock f) {
-		System.err
-				.println("something wrong! you can not define a function in a "
-						+ getClass().getSimpleName());
-	}
+    @Override
+    public void addFunctionDefinition(FunctionBlock f) {
+        System.err
+                .println("something wrong! you can not define a function in a "
+                        + getClass().getSimpleName());
+    }
 
-	@Override
-	public FunctionBlock getFunctionDefinition(String past, int nargs) {
-		return parent_.getFunctionDefinition(past, nargs);
-	}
+    @Override
+    public FunctionBlock getFunctionDefinition(String past, int nargs) {
+        return parent_.getFunctionDefinition(past, nargs);
+    }
 
 }
