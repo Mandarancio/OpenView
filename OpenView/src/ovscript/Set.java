f5d69739bf3df099bf42b7dd5452113dadff7784
Mandarancio <manda.mgf@gmail.com>
2014-05-22 23:23:46 +0200

    Set method up and run

--- OpenView/src/ovscript/Set.java
+++ OpenView/src/ovscript/Set.java
@@ -7,35 +7,36 @@ import core.Value;
 
 public class Set extends AbstractBlock {
 
-	private String objectName_;
-	private String nodeName_;
-	private Block block_;
+    private String objectName_;
+    private String nodeName_;
+    private Block block_;
 
-	private Set() {
-		super("Set");
-	}
+    private Set() {
+        super("Set");
+    }
 
-	public Set(String arg, Block block) {
-		this();
-		objectName_ = arg.split(":")[0];
-		nodeName_ = arg.split(":")[1];
-		block_ = block;
-	}
+    public Set(String arg, Block block) {
+        this();
+        objectName_ = arg.split(":")[0];
+        nodeName_ = arg.split(":")[1];
+        block_ = block;
+    }
 
-	@Override
-	public Value run(CodeBlock i) throws InterpreterException {
+    @Override
+    public Value run(CodeBlock i) throws InterpreterException {
 
-		OVComponent c = ObjectManager.getComponent(objectName_);
-		if (c != null) {
-			InNode n = c.getInNode(nodeName_);
-			if (n != null) {
-				Value v = block_.run(i);
-				n.trigger(v);
-			}
-		}
+        OVComponent c = ObjectManager.getComponent(objectName_);
+        if (c != null) {
+            InNode n = c.getInNode(nodeName_);
+            if (n != null) {
+                Value v = block_.run(i);
+                n.trigger(v);
+                return v;
+            }
+        }
 
-		throw new InterpreterException(
-				"no object or node found for set function!", getLine());
-	}
+        throw new InterpreterException(
+                "no object or node found for set function!", getLine());
+    }
 
 }
